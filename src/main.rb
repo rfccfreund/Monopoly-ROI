87827d3b84f8d7852be2a6ee680dd63d1b64ba71
rfccfreund <rmfreund@gmail.com>
2024-08-14 22:01:56 -0500

    created sim function and added to the engine

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,4 @@
 require './engine'
-require './forge'
 require 'pp'
 
 # Game loop:
@@ -14,14 +13,9 @@ require 'pp'
 properties = ["Med", "Baltic", "RR", "Ori Ave", "Vermont", "Connecticut", "Charles Place", "EE", "States Ave", "Virginia", "Penn Rail", "St. James",
 "Tenn Ave", "NY Ave", "Ken Ave", "Indiana", "Illinois", "B&O Rail", "Atlantic Ave", "Ventnor Ave", "WW", "Marvin Gardins", "Pacific Ave", "NC Ave", "Penn Ave",
 "Short Line", "Park Place", "Boardwalk"]
-all_returns = []
 avg_returns = []
 
-50.times do
-  players = create_players()
-  game_board = create_gameboard()
-  all_returns << play_game(players, 100, game_board)
-end
+all_returns = run_simulation()
 
 all_returns = all_returns.transpose
 all_returns.each do 
